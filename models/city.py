3b6b564a916a27c656b53678367cdb9502fef068
Ahmed Bentabet <61090548+ahmedbentabet@users.noreply.github.com>
2024-07-05 19:13:34 +0000

    Add task 8

--- models/city.py
+++ models/city.py
@@ -2,11 +2,16 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import Place  # Import Place to establish relationship
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """This class defines a city by various attributes"""
     __tablename__ = "cities"
 
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+    # Relationship with Place objects
+    places = relationship('Place', cascade='all, delete-orphan', backref='cities')
--- models/user.py
+++ models/user.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
+from models.place import Place  # Import Place to establish relationship
 
 
 class User(BaseModel, Base):
@@ -13,3 +15,6 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)  # not nullable
     first_name = Column(String(128))  # nullable
     last_name = Column(String(128))  # nullable
+
+    # Relationship with Place objects
+    places = relationship('Place', cascade='all, delete-orphan', backref='user')
