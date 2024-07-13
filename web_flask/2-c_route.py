7597a1c1b0e464938e18e5ba06db3ff4843ec486
Ahmed Bentabet <61090548+ahmedbentabet@users.noreply.github.com>
2024-07-13 12:23:49 +0000

    Add task 2 (Web framework)

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+"""
+This module contains a Flask web application with specific routes.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Display 'Hello HBNB!' when accessing the root route."""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display 'HBNB' when accessing the /hbnb route."""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """
+    Display 'C ' followed by the value of the text variable.
+    Replace underscore symbols with a space.
+    """
+    return 'C ' + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
