168855a97b9347b10ee46cbd1ef880ed59d7e325
Ahmed Bentabet <61090548+ahmedbentabet@users.noreply.github.com>
2024-07-13 12:36:09 +0000

    Add task 3 ( Web framework)

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,1 +1,44 @@
 #!/usr/bin/python3
+"""
+This module contains a Flask web application with specific routes.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Display 'Hello HBNB!' when accessing the root route."""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display 'HBNB' when accessing the /hbnb route."""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """
+    Display 'C ' followed by the value of the text variable.
+    Replace underscore symbols with a space.
+    """
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text):
+    """
+    Display 'Python ' followed by the value of the text variable.
+    Replace underscore symbols with a space.
+    Default value of text is 'is cool'.
+    """
+    return 'Python ' + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
