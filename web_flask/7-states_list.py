b7b77027435bde999d352b29007b24c9ce28b825
ahmedbentabet <bentabet841@gmail.com>
2024-07-25 13:10:03 +0000

    Fix: pycodestyle

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,10 +16,12 @@ def states_list():
     sorted_states = sorted(states, key=lambda x: x.name)
     return render_template(('7-states_list.html'), states=sorted_states)
 
+
 @app.teardown_appcontext
 def close_db(error):
     """Remove the current SQLAlchemy Session"""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
